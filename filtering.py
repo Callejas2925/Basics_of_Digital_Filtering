dd6f47786a309b4e8d80d7bfc7f708b37d24f650
Carlos Alberto Callejas Dominguez <49581243+Callejas2925@users.noreply.github.com>
2021-01-06 23:42:46 -0800

    Add files via upload

--- filtering.py
+++ filtering.py
@@ -1,6 +1,15 @@
 """
-Exploring Digital Filters
-    Circular Buffers
+Exploring FIR Filters
+    Low-Pass_Filters
+    1) Inverse Discrete Time Fourier Transform Method Used (IDTFT)
+    2) The design process takes place in the frequency domain (Normalized)
+    3) An ideal low pass filter frequency response is considered
+    4) The integral limits are -wc and wc (cutoff frequencies)
+    5) Solve integral to obtain the time domain response of filter
+    Note1: Convolution in the time domain is multiplication in the frequency domain and vice versa
+    Note2: L'Hopital's rule is used to find the limit as k approaches 0.
+
+    Circular Buffers (Performing Convolution)
     1) Circular Buffer is a technique used for emulating a shift register
     2) Circular Buffer uses an array instead of a shift register which makes
        shifting values in the buffer more difficult.
@@ -12,13 +21,69 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def lpf_taps(samples, wc):
+def low_pass_filter_taps(samples, wc):
+    """
+
+    :param samples: Integer k values (stem from -k to +k)
+    :param wc: Normalized Cut-Off Frequency (between -pi and pi)
+    :return: Low-Pass-Filter Coefficients
+    """
     taps = [0] * len(samples)
     for i in range(len(samples)):
         if i == int(len(samples) / 2):
             taps[i] = wc / math.pi
         else:
             taps[i] = math.sin(wc * samples[i]) / (math.pi * samples[i])
+    return taps
+
+
+def high_pass_filter_taps(samples, wc):
+    """
+
+    :param samples: Integer k values (stem from -k to +k)
+    :param wc: Normalized Cut-Off Frequency (between -pi and pi)
+    :return: High-Pass-Filter Coefficients
+    """
+    taps = [0] * len(samples)
+    for i in range(len(samples)):
+        if i == int(len(samples) / 2):
+            taps[i] = 1 - (wc / math.pi)
+        else:
+            taps[i] = - math.sin(wc * samples[i]) / (math.pi * samples[i])
+    return taps
+
+
+def band_pass_filter_taps(samples, wa, wb):
+    """
+
+    :param samples: Integer k values (stem from -k to +k)
+    :param wa: Low-End Cut-Off Frequency
+    :param wb: High-End Cut-Off Frequency
+    :return: Band-Pass Filter Coefficients
+    """
+    taps = [0] * len(samples)
+    for i in range(len(samples)):
+        if i == int(len(samples) / 2):
+            taps[i] = ((wb - wa) / math.pi)
+        else:
+            taps[i] = (math.sin(wb * samples[i]) - math.sin(wa * samples[i])) / (math.pi * samples[i])
+    return taps
+
+
+def band_stop_filter_taps(samples, wa, wb):
+    """
+
+    :param samples: Integer k values (stem from -k to +k)
+    :param wa: Low-End Cut-Off Frequency
+    :param wb: High-End Cut-Off Frequency
+    :return: Band-Stop-Filter Coefficients
+    """
+    taps = [0] * len(samples)
+    for i in range(len(samples)):
+        if i == int(len(samples) / 2):
+            taps[i] = 1 - ((wb - wa) / math.pi)
+        else:
+            taps[i] = - (math.sin(wb * samples[i]) - math.sin(wa * samples[i])) / (math.pi * samples[i])
     return taps
 
 
@@ -93,18 +158,38 @@ def plot(x_axis, y_axis):
 
 fs = 1000  # Sampling Freq
 fc = 100   # Cut-off Freq
+fc2 = 200
 w = 2 * math.pi * (fc / fs)
+wl = 2 * math.pi * (fc / fs)
+wh = 2 * math.pi * (fc2 / fs)
 
 N = 40
 t = sample_taps(size=N)
 impulse = [0]*1001
 impulse[int(len(impulse)/2)] = 1
 
-ilpf = lpf_taps(samples=t, wc=w)
+ilpf = low_pass_filter_taps(samples=t, wc=w)
+ihpf = high_pass_filter_taps(samples=t, wc=w)
+ibpf = band_pass_filter_taps(samples=t, wa=wl, wb=wh)
+ibsf = band_stop_filter_taps(samples=t, wa=wl, wb=wh)
+
 con_ilpf = circular_convolution(input_signal=impulse, impulse_response_coefficients=ilpf)
-fft_ilpf = fft_db(con_ilpf)
+con_ihpf = circular_convolution(input_signal=impulse, impulse_response_coefficients=ihpf)
+oon_ibpf = circular_convolution(input_signal=impulse, impulse_response_coefficients=ibpf)
+con_ibsf = circular_convolution(input_signal=impulse, impulse_response_coefficients=ibsf)
 
-f = freq_taps(function=fft_ilpf, sample_freq=fs)
+fft_ilpf = fft_db(function=con_ilpf)
+fft_ihpf = fft_db(function=con_ihpf)
+fft_ibpf = fft_db(function=oon_ibpf)
+fft_ibsf = fft_db(function=con_ibsf)
 
-plt.plot(f, fft_ilpf)
+f1 = freq_taps(function=fft_ilpf, sample_freq=fs)
+f2 = freq_taps(function=fft_ihpf, sample_freq=fs)
+f3 = freq_taps(function=fft_ibpf, sample_freq=fs)
+f4 = freq_taps(function=fft_ibsf, sample_freq=fs)
+
+plt.plot(f1, fft_ilpf)
+plt.plot(f2, fft_ihpf)
+plt.plot(f3, fft_ibpf)
+plt.plot(f4, fft_ibsf)
 plt.show()
